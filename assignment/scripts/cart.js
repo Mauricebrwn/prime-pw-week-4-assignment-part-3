8d772370be102864a7d4649fed1c693980bda26d
Maurice Brown <mauricebrwn6890@gmail.com>
2022-11-20 00:48:07 -0600

    additem

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item){
+    console.log('addItem', item );
+    basket.push(item);
+    if (addItem){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+addItem('milk');
+addItem('cheese');
+addItem('grapes');
+console.log(`Basket is ${basket}`);
+console.log('Adding butter (expect true)', addItem('butter'));
+console.log(`Basket is now ${basket}`);
+
+function listItems(){
+    console.log(basket);
+}
